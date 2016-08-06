116466356ba92c2f92e6c857287f9fb0e0224b2f
trophies2015 <=>
2016-08-06 13:33:04 -0400

    gu

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
   // $link = mysqli_connect ("localhost:8000", "yash1", "forest");
    $t = $_GET["img"];
-   $ran = (string)md5(mt_rand());
-   $ran = "images/" . $ran;
-   $command = "echo \"" . $t . "\" > " . $ran . ".jpg";
+   $x_cor = $_GET["x_cor"];
+   $y_cor = $_GET["y_cor"];
+   $ran = "images/" . (string)$x_cor . "," . (string)$y_cor;
+   $command = "echo \"" . $t . "\" > " . $ran;
    system($command);
      
 ?>
